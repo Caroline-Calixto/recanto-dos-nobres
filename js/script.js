9a873d3d80c7a73aecf1f6c353d859865ae46bab
Caroline Calixto <79118657+Caroline-Calixto@users.noreply.github.com>
2023-09-02 22:01:07 -0300

    inclusão de services, fotos e responsividade

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 40,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        740: {
-            slidesPerView: 1,
-        }
-    }
-});
 
 function showMenu() {
     let menuMobile = document.querySelector('.mobile-menu');
